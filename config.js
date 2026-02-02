cb948b250571a732e352a26cb91f8b5af1483c43
hivafaghihi02-crypto <hiva.faghihi02@gmail.com>
2026-02-02 14:32:08 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Hajer",
+    valentineName: "Hajer 🥰😻🥺",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
