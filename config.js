8fcc4cdb4a5cce16cf64044b7016a2982a3c6716
hivafaghihi02-crypto <hiva.faghihi02@gmail.com>
2026-02-02 15:30:18 -0500

    Update config.js

--- config.js
+++ config.js
@@ -81,7 +81,7 @@ const CONFIG = {
         musicUrl: "https://res.cloudinary.com/dtph2dujp/video/upload/v1770060055/Syd_-_Out_Loud_Official_Audio_ft_Kehlani_b5x8i9.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
-        volume: 0.5                        // Volume level (0.0 to 1.0)
+        volume: 1.0                        // Volume level (0.0 to 1.0)
     }
 };
 
